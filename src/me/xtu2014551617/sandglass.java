01e0001a56d0631e92830598e4fa26fe96b2cdea
xtu2014551617 <1578574089@qq.com>
2018-05-30 15:09:12 +0800

    Create sandglass.java

---
+++ src/me/xtu2014551617/sandglass.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int tCase = in.nextInt();
+        while (tCase-- != 0) {
+            int row = in.nextInt();
+            for (int i = 0; i < row; i++) {
+                if (i == 0 || i == row - 1) {
+                    printTopOrBottom(row);
+                } else if (i >= 1 && i <= row / 2 - 1) {
+                    printTopBody(i, row);
+                } else {
+                    printBottomBody(i, row);
+                }
+            }
+        }
+    }
+
+    private static void printTopOrBottom(int row) {
+        printContent('-', row - 1);
+        printContent('\n', 1);
+    }
+
+    private static void printTopBody(int no, int row) {
+        printContent(' ', no - 1);
+        printContent('\\', 1);
+        if (no == 1) {
+            printContent(' ', row - no * 2 - 1);
+        } else {
+            printContent('*', row - no * 2 - 1);
+        }
+        printContent('/', 1);
+        printContent('\n', 1);
+    }
+
+    private static void printBottomBody(int no, int row) {
+        printContent(' ',(row - no - 1) - 1);
+        printContent('/', 1);
+        printContent(' ', row/2 - (row - no - 1) - 1);
+        printContent('*', 1);
+        printContent(' ', row/2 - (row - no - 1) - 1);
+        printContent('\\', 1);
+        printContent('\n', 1);
+    }
+
+    private static void printContent(char character, int len) {
+        for (int i = 0; i < len; i++) {
+            System.out.print(character);
+        }
+    }
+}
