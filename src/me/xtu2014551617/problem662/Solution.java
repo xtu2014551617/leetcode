a48969c8a6f0bf1d5acb58e7d938a44c2305b9d2
sherlockmoon <1578574089@qq.com>
2019-08-20 23:15:45 +0800

    add problem662

--- src/me/xtu2014551617/problem662/Solution.java
+++ src/me/xtu2014551617/problem662/Solution.java
@@ -16,31 +16,34 @@ public class Solution {
 
     public static class MyTreeNode {
         TreeNode treeNode;
-        int pos;
-        MyTreeNode(TreeNode treeNode, int pos) {
+        int pos, depth;
+        MyTreeNode(TreeNode treeNode, int pos, int depth) {
             this.treeNode = treeNode;
             this.pos = pos;
+            this.depth = depth;
         }
     }
 
     public int widthOfBinaryTree(TreeNode root) {
-        if (root != null) {
-            root.val = 1;
+        if (root == null) {
+            return 0;
         }
         LinkedList<MyTreeNode> linkedList = new LinkedList<>();
-        linkedList.add(new MyTreeNode(root, 1));
+        linkedList.add(new MyTreeNode(root, 0, 0));
         int ans = 0;
+        int lastWeight = 0;
+        int lastPos = 0;
         while (!linkedList.isEmpty()) {
             MyTreeNode head = linkedList.pop();
-            if (head != null) {
-                int height = head.treeNode.val + 1;
-                if (head.treeNode.left != null) head.treeNode.left.val = height;
-                if (head.treeNode.right != null) head.treeNode.right.val = height;
-                linkedList.add(new MyTreeNode(head.treeNode.left, 2 * head.pos));
-                linkedList.add(new MyTreeNode(head.treeNode.left, 2 * head.pos + 1));
-                if ()
-                ans = Math.max(ans, head.pos - left + 1);
+            if (head.treeNode == null)
+                continue;
+            linkedList.add(new MyTreeNode(head.treeNode.left, 2 * head.pos, head.depth + 1));
+            linkedList.add(new MyTreeNode(head.treeNode.right, 2 * head.pos + 1, head.depth + 1));
+            if (lastWeight != head.depth) {
+                lastWeight = head.depth;
+                lastPos = head.pos;
             }
+            ans = Math.max(ans, head.pos - lastPos + 1);
         }
         return ans;
     }
